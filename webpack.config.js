e2892b39b862c42b75244e70473ed9fae102e542
m.yunuz <m.yunuz@adcash.com>
2024-09-17 20:10:34 +0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+  //   devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./"),
+    },
+    host: "localhost",
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
