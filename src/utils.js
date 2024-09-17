b86d40980133557eab004029114d6ad61a2ee75e
m.yunuz <m.yunuz@adcash.com>
2024-09-17 20:21:25 +0300

    Add utils

---
+++ src/utils.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param {HTMLElement} element
+ * @param {Event} event
+ * @param {Function} callback
+ * @returns
+ */
+export const addEvent = (element, event, callback) => {
+  if (element.addEventListener) {
+    return element.addEventListener(event, callback);
+  }
+  return element.attachEvent(`on${event}`, callback);
+};
+
+/**
+ *
+ * @param {HTMLElement} element
+ * @param {Event} event
+ * @param {Function} callback
+ * @returns
+ */
+export const removeEvent = (element, event, callback) => {
+  if (element.removeEventListener) {
+    return element.removeEventListener(event, callback);
+  }
+  element.detachEvent(`on${event}`, callback);
+};
