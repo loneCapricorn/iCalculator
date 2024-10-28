3d2009f99a417693b82bbd9a7bab5762077b0498
m.yunuz <m.yunuz@adcash.com>
2024-10-28 22:33:15 +0200

    Add constants

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const NUMBER_BTNS_IDS = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+];
+
+export const SPECIAL_BTNS_IDS = [
+  'clear',
+  'plusMinus',
+  'percent',
+  'division',
+  'multiplication',
+  'subtraction',
+  'addition',
+  'dot',
+  'equals',
+];
+
+export const DISPLAY_ID = 'display';
