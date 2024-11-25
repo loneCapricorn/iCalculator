1abf0181964dfa3de30f2d23d3b9cb646ec89a5e
loneCapricorn <yyunuzmehmed@gmail.com>
2024-11-25 18:55:49 +0200

    Add loaders and plugin to generate output files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Calculator from './Calculator.js';
+import './styles/index.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Calculator();
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production', // development | production | none
   entry: {
-    bundle: path.resolve(__dirname, "src/index.js"),
+    bundle: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
     clean: true,
   },
   //   devtool: "source-map",
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "./"),
+      directory: path.resolve(__dirname, 'dist'),
     },
-    host: "localhost",
+    host: 'localhost',
     port: 3000,
     open: true,
     hot: true,
@@ -23,15 +24,26 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'iCalculator',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
 };
